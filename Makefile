76ff581f9c207d9c69a74803540c5fdd18b4708e
Tilo Wiedera <tilo@wiedera.de>
2014-03-11 14:39:58 +0100

    skeleton

---
+++ Makefile
@@ -0,0 +1,42 @@
+include Makefile.config
+CC = g++
+ifeq ($(DEBUG), 1)
+  OGDF_LIBRARY_DIR = $(OGDF_LIBRARY_DIR_DEBUG)
+else
+  OGDF_LIBRARY_DIR = $(OGDF_LIBRARY_DIR_RELEASE)
+endif
+INCLUDES = -I$(OGDF_INCLUDE_DIR)
+CXXFLAGS = -std=c++11 $(INCLUDES) -Wall -Wno-unused-parameter
+LDFLAGS = -pthread -lm -L$(OGDF_LIBRARY_DIR)
+ifeq ($(DEBUG),1)
+  CXXFLAGS += -O0 -g -ggdb -W -DOGDF_DEBUG
+else
+  ifeq ($(DEBUG), 2)
+    # profiler mode but release
+    CXXFLAGS += -O3 -g -ggdb -pg
+    LDFLAGS += -pg
+  else
+    CXXFLAGS += -O3
+  endif
+endif
+LDFLAGS = -pthread -lm -L$(OGDF_LIBRARY_DIR)
+LDLIBS = -lOGDF -lCOIN
+
+BINARIES = stp-solver
+OBJS = 
+
+all: $(BINARIES)
+
+stp-solver: stp-solver.o
+
+%o: %cc
+
+Makefile.config:
+	$(error Need to know OGDF, CPLEX and CONCERT paths. Please rename Makefile.config.default to Makefile.config and edit appropriately)
+
+.PHONY: clean distclean
+clean:
+	$(RM) $(BINARIES) $(BINARIES:%=%.o) $(OBJS)
+
+distclean: clean
+	$(RM) Makefile.config
---
+++ stp-solver.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+#include <ogdf/basic/Graph.h>
+#include <ogdf/basic/List.h>
+#include <ogdf/basic/EdgeArray.h>
+#include <ogdf/basic/NodeArray.h>
+#include <ogdf/fileformats/GraphIO.h>
+
+using namespace ogdf;
+
+/**
+ * Outputs a text describing how to use this program.
+ */
+void printHelp() {
+	std::cout << "You need to provide a STP file containing the "
+	  "Steiner-tree problem. The algorithm will then find the optimal "
+          "Solution and print the total cost." << endl;
+}
+
+/**
+ * Yields a optimal steiner tree for the given STP instance.
+ * Applies Shore, Foulds and Gibbons' branch and bound algorithm.
+ *
+ * \param graph
+ *	the Graph containing both steiner and terminal nodes
+ * \param weight
+ *	the weight of each edge
+ * \param terminals
+ *	the set of terminal nodes, as opposed to steiner nodes
+ * \param isTerminal
+ *	mapping for each node whether it is a terminal or a steiner node
+ * \param tree
+ *	will hold the resulting tree
+ *
+ * \return
+ *	the total cost of the steiner tree
+ */
+double calcSolution(Graph &graph, EdgeArray<double> weight, List<node> &terminals, NodeArray<bool> &isTerminal,  Graph &tree) {
+	return 0;
+}
+
+/**
+ * Checks the provided command line arguments for validity.
+ * Executes the b&b algorithm and writes the result to SVG and
+ * command line.
+ */
+int main(int argc, char* argv[]) {
+	if(argc != 2) {
+		printHelp();
+	}
+	else {
+		EdgeWeightedGraph<double> graph;
+		List<node> terminals;
+		NodeArray<bool> isTerminal(graph);
+		if(GraphIO::readSTP(graph, terminals, isTerminal, argv[1])) {
+		}
+		else {
+			std::cerr << "unable to read file: " << argv[1] << endl;
+		}
+	}
+	return 0;
+}
